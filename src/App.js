49d3f6e032b1b67fe61c47fee4c3c1352da2d4b6
Darek Podczasik <boostertech@mail.com>
2024-07-14 14:05:20 +0100

    inputs added buttons added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
-import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [inputValue, setInputValue] = useState(0);
+  const [count, setCount] = useState(0);
+
+  const date = new Date();
+  date.setDate(date.getDate() + inputValue);
+
+  function handleInputChange(e) {
+    setInputValue(Number(e.target.value));
+  }
+
+  function handleRangeChange(e) {
+    setCount(Number(e.target.value));
+  }
+
+  return (
+    <div>
+      <input type="range" min="1" max="10" onChange={handleRangeChange}></input>
+      <p>{count}</p>
+      <button onClick={() => setInputValue(inputValue + count)}>+</button>
+
+      <input
+        value={inputValue}
+        type="number"
+        onChange={handleInputChange}
+      ></input>
+      <button onClick={() => setInputValue(inputValue - count)}>-</button>
+      <p>
+        {date.toLocaleDateString(undefined, {
+          weekday: "short",
+          month: "short",
+          day: "2-digit",
+          year: "numeric",
+        })}
+      </p>
+      <button onClick={() => setInputValue(0)}>Reset</button>
+    </div>
+  );
 }
 
 export default App;
