42764f6eaed401c6dd6672123f91e3f71ac5106a
Darek Podczasik <boostertech@mail.com>
2024-07-27 12:23:12 +0100

    <DateCounter/> created, styles added

---
+++ src/components/DateCounter/styled.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.header`
+  display: flex;
+  text-decoration: dotted;
+`;
+
+export const Image = styled.img`
+  max-width: 100px;
+  margin-right: 20px;
+`;
+
+export const Text = styled.h1`
+  
+`;
