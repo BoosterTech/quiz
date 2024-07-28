f5ba41462b96544b23172b1eb7220de36fecb24b
Darek Podczasik <boostertech@mail.com>
2024-07-28 14:53:35 +0100

    <ProgressBar/> added maxPossiblePoints created

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Error from "./components/Error";
 import StartScreen from "./components/StartScreen";
 import Question from "./components/Question";
 import NextButton from "./components/NextButton";
+import ProgressBar from "./components/ProgressBar";
 
 const initialState = {
   questions: [],
@@ -51,11 +52,15 @@ function reducer(state, action) {
 }
 
 function App() {
-  const [{ questions, status, index, answer }, dispatch] = useReducer(
+  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
     reducer,
     initialState
   );
   const numQuestions = questions.length;
+  const maxPossiblePoints = questions.reduce(
+    (prev, curr) => prev + curr.points,
+    0
+  );
 
   useEffect(function () {
     fetch("http://localhost:9000/questions")
@@ -75,6 +80,13 @@ function App() {
         )}
         {status === "active" && (
           <>
+            <ProgressBar
+              index={index}
+              numQuestions={numQuestions}
+              points={points}
+              maxPossiblePoints={maxPossiblePoints}
+              answer={answer}
+            />
             <Question
               question={questions[index]}
               dispatch={dispatch}
