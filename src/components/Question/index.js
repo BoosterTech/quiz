cd67306ce48221769f8568d8a538d12ed1bc030f
Darek Podczasik <boostertech@mail.com>
2024-07-28 14:04:38 +0100

    <Question/> updated props added (question, dispatch, answer)

--- src/components/Question/index.js
+++ src/components/Question/index.js
@@ -1,3 +1,13 @@
-export default function Question() {
-  return <p>Question</p>;
+import Options from "../Options";
+
+export default function Question({ question, dispatch, answer }) {
+  console.log(question);
+  return (
+    <>
+      <h3>{question.question}</h3>{" "}
+      <div>
+        <Options question={question} dispatch={dispatch} answer={answer} />
+      </div>
+    </>
+  );
 }
