a34d60010779a7a8b402eb3a7e12aad56ac1655d
Darek Podczasik <boostertech@mail.com>
2024-07-29 22:05:47 +0100

    Highscore created and added to display

--- src/components/FinishScreen/index.js
+++ src/components/FinishScreen/index.js
@@ -1,21 +1,22 @@
-function FinishScreen({ points, maxPossiblePoints }) {
+function FinishScreen({ points, maxPossiblePoints, highscore }) {
   const percentage = (points / maxPossiblePoints) * 100;
 
   let emoji = "";
   if (percentage === 100) emoji = "🎖️";
-
   if (percentage >= 80 && percentage < 100) emoji = "🥳";
   if (percentage >= 50 && percentage < 80) emoji = "😀";
-
   if (percentage >= 0 && percentage < 50) emoji = "😊";
   if (percentage === 0) emoji = "😒";
 
   if (percentage)
     return (
-      <p>
-        <span>{emoji}</span> You scored <strong>{points}</strong> points out of{" "}
-        {maxPossiblePoints} ({Math.ceil(percentage)}%)
-      </p>
+      <>
+        <p>
+          <span>{emoji}</span> You scored <strong>{points}</strong> points out
+          of {maxPossiblePoints} ({Math.ceil(percentage)}%)
+        </p>
+        <p>Highscore: {highscore} points</p>
+      </>
     );
 }
 
