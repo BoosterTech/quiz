1110a9b2f1d12fda90a15025da7f1ca9d06d5930
Darek Podczasik <boostertech@mail.com>
2024-08-05 15:26:26 +0100

    styles moved from <DataCounter/> to correct file

--- src/components/DateCounter/styled.js
+++ src/components/DateCounter/styled.js
@@ -1,15 +1,1 @@
-import styled from "styled-components";
 
-export const Wrapper = styled.header`
-  display: flex;
-  text-decoration: dotted;
-`;
-
-export const Image = styled.img`
-  max-width: 100px;
-  margin-right: 20px;
-`;
-
-export const Text = styled.h1`
-  
-`;
