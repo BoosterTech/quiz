33864e3f45a57ad7966b8a45f7f8dae2d228ff33
Darek Podczasik <boostertech@mail.com>
2024-08-05 14:57:38 +0100

    styles added to <Main/>

--- src/components/Main/styled.js
+++ src/components/Main/styled.js
@@ -1,3 +1,9 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  position: absolute;
+  color: #fff;
+ text-align: center;
+  left: 50%;
+  transform: translate(-50%);
+`;
