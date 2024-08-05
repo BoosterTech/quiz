28421a023763130aa6eb17e6862774a19efbb1af
Darek Podczasik <boostertech@mail.com>
2024-08-05 15:26:56 +0100

    <StartScreen/> styled added and applied

--- src/components/StartScreen/index.js
+++ src/components/StartScreen/index.js
@@ -1,10 +1,13 @@
+import { Wrapper } from "./styled";
+import { Button, H2, H3 } from "./styled";
+
 function StartScreen({ numQuestions, dispatch }) {
   return (
-    <div>
-      <h2>Welcome to The React Quiz!</h2>
-      <h3>{numQuestions} questions to test your React mastery</h3>
-      <button onClick={() => dispatch({ type: "start" })}>Let's start</button>
-    </div>
+    <Wrapper>
+      <H2>Welcome to The React Quiz!</H2>
+      <H3>{numQuestions} questions to test your React mastery</H3>
+      <Button onClick={() => dispatch({ type: "start" })}>Let's start</Button>
+    </Wrapper>
   );
 }
 
---
+++ src/components/StartScreen/styled.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  margin-top:0;
+`;
+
+export const H2 = styled.h2`
+  font-size: 1.8rem;
+  margin-bottom: 0;
+`;
+
+export const H3 = styled.h3`
+margin-top: 6px;
+  font-size: 1.1rem;
+  font-weight: 500;
+`;
+
+export const Button = styled.button`
+  background-color: #5d5656;
+  color: #fff;
+  border-radius: 25px;
+  padding: 0.6rem 1rem;
+  border: none;
+  margin-top: 10px;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
